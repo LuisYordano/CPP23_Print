91c3e383e491e52cbd8b1aedf954ebecd93b4eb9
Luis Yordano <lycruzc@yahoo.com>
2023-05-20 09:47:16 -0500

    Add project files.

---
+++ ConsoleApplicationPrint/ConsoleApplicationPrint.cpp
@@ -0,0 +1,33 @@
+// ConsoleApplicationPrint.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//#include <iostream>
+import std;
+
+int main()
+{
+    //std::cout << "Hello World 2023!\n";
+
+    //std::print("{0} {2}{1}!\n", "Hello", 23, "C++"); // overload (1)
+
+    // Each call to std::println ends with new-line
+    std::println("Please");
+    std::println("enter");
+
+    std::print("pass");
+    std::print("word");
+
+    std::println(""); // same effect as std::print("\n");
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
